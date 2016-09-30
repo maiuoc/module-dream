1fb1ec4380d7f553bc3b8bf362a802c3144ac34b
maiuoc <maiuoc.k8c@gmail.com>
2016-09-30 13:23:35 +0700

    remove fle

--- view/frontend/web/js/module1.js
+++
@@ -1,9 +0,0 @@
-define([], function () {
-    var returnedModule = function () {
-        var _name = 'module1 name';
-        this.getName = function () {
-            return _name;
-        }
-    };
-    return returnedModule;
-});
